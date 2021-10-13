97f2f85e05c02fcf6c6227678b254eafc472cb39
TraceGSheets <tracegsheets@gmail.com>
2021-10-13 00:17:06 -0500

    setting up backend not finished yet

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://trace:Trace123@merntheszns.rxrnn.mongodb.net/MernTheSZNS?retryWrites=true&w=majority'
+}
---
+++ routes/api/items.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+// item model
+const Item = require('../../models/Item');
+
+// @route GET api/items
+// @desc Get all items
+// @access Public
+router.get('/', (req, res) => {
+    Item.find()
+    .sort({ date: -1 })
+    .then(items => res.json(items))
+});
+
+// @route POST api/items
+// @desc Create a post
+// @access Public
+router.post('/', (req, res) => {
+    const newItem = new Item({
+        name: request.body.name
+    });
+
+    newItem.save().then(item => res.json(item));
+});
+
+
+module.exports = router;
